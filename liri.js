d80bf752b859ce2ba525cc5254babe7529c0b3f7
Emmy Emme <masteremmy.js@gmail.com>
2017-07-24 01:12:20 -0400

    Built functionality for spotify-this-song and movie-this and called them in the switch case fcn

--- liri.js
+++ liri.js
@@ -1,18 +1,27 @@
-const keys = require('./keys.js');
 
+//**DEPENDENCIES**//
+
+//Node Packages 
+
+const request = require('request'); 
 const Twitter = require('twitter');
- 
+const Spotify = require('node-spotify-api');
+
+
+// Keys 
+
+const keys = require('./keys.js');
+const spotify = new Spotify(keys.spotifyKeys);
+const client = new Twitter(keys.twitterKeys);
+
+
+//**TWITTER**// 
+
 const getMyTweets = function() {
 
-const client = new Twitter(keys.twitterKeys);
- 
-const params = {screen_name: 'SchwiftySauce'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  	if (!error) {
-  		// var tweetsObject = JSON.stringify(tweets, null, 2);
-  		// tweets.forEach(function(tweet) {
-    	// console.log(tweet.text);
-    //loop thru all the tweets in the array and console.log the data 
+	const params = {screen_name: 'SchwiftySauce'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  	if (!error) {
     	for (var i = 0; i < tweets.length; i++) {
     		console.log(tweets[i].created_at); 
     		console.log(' '); 
@@ -22,13 +31,56 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	});
 }
 
+
+//**SPOTIFY**// 
+
+const getMySpotify = function (functionData) {
+ 
+	spotify.search({type: 'track', query: functionData}, function(err, data) {
+	    if (err) {
+	        console.log('Error occurred: ' + err);
+	        return;
+	    }
+	 		data.tracks.items.forEach(function(item)
+	 	{
+
+		 	console.log(item.name);
+		    console.log(item.album.name);
+		    console.log(item.artists[0].name);
+		    console.log(item.preview_url);
+		    console.log('-------------');		
+
+	 	});
+	});
+}
+
+//**ONLINE MOVIE DATABASE**// 
+
+const getMyMovie = function (movieName) {
+
+	request('http://www.omdbapi.com/?t='+ movieName + '&apikey=40e9cece', function(error, response, body){
+		if(!error && response.statusCode == 200){
+			console.log(body);
+		} else {
+			console.log(' :(');
+		}
+	})
+}
+
+
 let pick = function(caseData, functionData) {
 	switch(caseData) {
 		case 'my-tweets': 
-		getMyTweets(); 
-		break; 
+			getMyTweets(); 
+			break; 
+		case 'spotify-this-song':
+			getMySpotify(functionData);
+			break;
+		case 'movie-this': 
+			getMyMovie(functionData);
+			break;  
 		default: 
-		console.log('LIRI does not know that'); 
+			console.log('LIRI does not know that'); 
 	}
 }
 
@@ -38,6 +90,4 @@ let runThis = function(argOne, argTwo) {
 }; 
 
 runThis(process.argv[2], process.argv[3]); 
-
-
 
