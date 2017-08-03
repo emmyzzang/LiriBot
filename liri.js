0881b737dd16a8ee0d85c8d5da0ce8b2ae97138b
Emmy Emme <masteremmy.js@gmail.com>
2017-08-03 01:17:52 -0400

    Updated fs and debugged switch case

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@
 const request = require('request'); 
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
-
+const fs = require('fs');
 
 // Keys 
 
@@ -26,7 +26,9 @@ const getMyTweets = function() {
     		console.log(tweets[i].created_at); 
     		console.log(' '); 
     		console.log(tweets[i].text); 
+    		fs.appendFile('random.txt', tweets[i].text + " Created on: " + tweets[i].created_at + "\n");
 	    	}
+	    fs.appendFile('random.txt', "============================================================\n");
   		}
 	});
 }
@@ -44,12 +46,15 @@ const getMySpotify = function (functionData) {
 	 		data.tracks.items.forEach(function(item)
 	 	{
 
-		 	console.log(item.name);
-		    console.log(item.album.name);
-		    console.log(item.artists[0].name);
-		    console.log(item.preview_url);
-		    console.log('-------------');		
-
+    console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+    console.log("The Song's Name: " + data.tracks.items[0].name);
+    console.log("A Preview Link of the Song: " + data.tracks.items[0].external_urls.spotify);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    fs.appendFile('random.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + 
+    	"Song Name: " + data.tracks.items[0].name + "\n" + 
+    	"Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n" + 
+    	"Album: " +
+      data.tracks.items[0].album.name + "\n" + "====================================================\n");
 	 	});
 	});
 }
@@ -60,7 +65,15 @@ const getMyMovie = function (movieName) {
 
 	request('http://www.omdbapi.com/?t='+ movieName + '&apikey=40e9cece', function(error, response, body){
 		if(!error && response.statusCode == 200){
-			console.log(body);
+	  console.log("Title: " + JSON.parse(body).Title);
+      console.log("Released: " + JSON.parse(body).Released);
+      console.log("Imdb Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoUserRating);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      fs.appendFile('random.txt', "\nTitle: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating: " + JSON.parse(body).tomatoUserRating + "\nCountry: " + JSON.parse(body).Country +
+        "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors + "\n======================================================");
 		} else {
 			console.log(' :(');
 		}
@@ -79,9 +92,24 @@ let pick = function(caseData, functionData) {
 		case 'movie-this': 
 			getMyMovie(functionData);
 			break;  
+		case "do-what-it-says":
+    		doThis();
+    		break;
 		default: 
 			console.log('LIRI does not know that'); 
 	}
+}
+
+function doThis() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      console.log(error);
+    } else {
+      let dataArray = data.split(",");
+      let one = dataArray[0];
+      let two = dataArray[1];
+    }
+  });
 }
 
 //pass the arguments from the user 
