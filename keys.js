5cd790860e4885a46026d6aa549f370711694542
Emmy Emme <masteremmy.js@gmail.com>
2017-10-01 13:34:51 -0400

    the return of the keys!

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+
+exports.twitterKeys = {
+	 consumer_key: 'mVveOau4nxJxYIPXbJEfznHop', 
+	 consumer_secret: 'DMBEJv61FCx8yNZD5omltoVNEkAxht0yFiBlZJY2DMJzObEsVy',
+	 access_token_key: '257093528-RujKjNJDmTuBaNeVtpOCKAiPfLCThOjyhqvfA0IY',
+	 access_token_secret: '5ZzqMVtBI7n1LfHOb5nkh45uTjweCLUKStCVJy5uHRMKn'
+}
+
+exports.spotifyKeys = {
+	id: 'a1c0889c3c374afc8429b943550e8277',
+	secret: '86b0bcea2f19409cb745468d00bf3652'
+}
